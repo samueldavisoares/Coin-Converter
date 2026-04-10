acadedb33e99a7dfd05bf31ee706f1216cb7f3e0
samueldavisoares <samuel.daviss878@gmail.com>
2026-04-09 23:48:21 -0300

    first-commit

---
+++ scripts.js
@@ -0,0 +1,47 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+
+function convertValues() {                                             //so o valor do input
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".value")
+    const currencyValueToConverted = document.querySelector(".value-to-convert")
+
+    const dolarToday = 5.15
+    const euroToday = 5.96
+
+    if (currencySelect.value == "dolar") { // se o valor selecionado for dolar, entre aqui
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro") { // se o valor selecionado for euro, entre aqui
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+    // para formatar o valor do input para o formato de moeda brasileira
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+}
+
+function changeCurrency() {
+    const currencyName = document.querySelector("#currency-name")
+
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dolar"
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+    }
+}
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
